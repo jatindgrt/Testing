6ff183419dc09aabbc5b7ff84c5e9b051b09f02d
jatindgrt <jatinarora2797@gmail.com>
2024-04-08 13:22:09 +0530

    first commit

---
+++ src/test/java/stepdefinition/Stepdef.java
@@ -0,0 +1,43 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stepdef {
+	
+	@Given("user Launch Chrome Browser")
+	public void user_launch_chrome_browser() {
+
+	}
+
+	@When("User opens url {string}")
+	public void user_opens_url(String string) {
+	    
+	}
+
+	@When("User enter Email as {string} and Password as {string}")
+	public void user_enter_email_as_and_password_as(String string, String string2) {
+	   
+	}
+
+	@When("Click on Login")
+	public void click_on_login() {
+	   
+	}
+
+	@Then("Page Title should be {string}")
+	public void page_title_should_be(String string) {
+	   
+	}
+
+	@When("User click on the log out link")
+	public void user_click_on_the_log_out_link() {
+	    
+	}
+
+	@When("Close the Browser")
+	public void close_the_browser() {
+	    
+	}
+	}
